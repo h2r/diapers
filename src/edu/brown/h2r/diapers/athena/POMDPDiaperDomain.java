9e40fb55be6243f56d380aec8536ce6f3d1dc858
Izaak Baker <iebaker@cs.brown.edu>
2013-12-10 10:38:13 -0500

    Wrote some of POMDPDiaperDomain

--- src/edu/brown/h2r/diapers/athena/POMDPDiaperDomain.java
+++ src/edu/brown/h2r/diapers/athena/POMDPDiaperDomain.java
@@ -26,7 +26,18 @@ public class POMDPDiaperDomain implements DomainGenerator {
  * ========================================================================= */
 
 	public Domain generateDomain() {
-		Domain domain = new POMDPDomain();
+
+		Domain domain = new POMDPDomain() {
+			@Override
+			public POMDPState getExampleState() {
+				return POMDPDiaperDomain.getNewState(this);
+			}
+
+			@Override
+			public List<State> getAllStates() {
+				return POMDPDiaperDomain.getAllStates(this);
+			}
+		}
 
 		Attribute attrMentalState = new Attribute(domain, P.ATTR_MENTAL_STATE, Attribute.AttributeType.RELATIONAL);
 
@@ -38,17 +49,20 @@ public class POMDPDiaperDomain implements DomainGenerator {
 		Action advanceAction = new AdvanceAction(domain, P.ACTION_ADVANCE);
 		Action speakAction = new SpeakAction(domain, P.ACTION_SPEAK);
 
-		Observation stateAObservation = new SimpleStateObservation(domain, P.OBS1, P.OBJ_STATE_A);
-		Observation stateBObservation = new SimpleStateObservation(domain, P.OBS2, P.OBJ_STATE_B);
-		Observation stateCObservation = new SimpleStateObservation(domain, P.OBS3, P.OBJ_STATE_C);
-		Observation stateDObservation = new SimpleStateObservation(domain, P.OBS4, P.OBJ_STATE_D);
-		Observation stateEObservation = new SimpleStateObservation(domain, P.OBS5, P.OBJ_STATE_E);
+		Observation stateXObservation = new SimpleStateObservation(domain, P.OBS_STATE_X, P.OBJ_STATE_X);
+		Observation stateAObservation = new SimpleStateObservation(domain, P.OBS_STATE_A, P.OBJ_STATE_A);
+		Observation stateBObservation = new SimpleStateObservation(domain, P.OBS_STATE_B, P.OBJ_STATE_B);
+		Observation stateCObservation = new SimpleStateObservation(domain, P.OBS_STATE_C, P.OBJ_STATE_C);
+		Observation stateDObservation = new SimpleStateObservation(domain, P.OBS_STATE_D, P.OBJ_STATE_D);
+		Observation stateEObservation = new SimpleStateObservation(domain, P.OBS_STATE_E, P.OBJ_STATE_E);
+		Observation stateYObservation = new SimpleStateObservation(domain, P.OBS_STATE_Y, P.OBJ_STATE_Y);
+		Observation nullObservation = new Observation(domain, P.NULL_OBSERVATION);
 
 		return domain;
 	}
 
 /* ============================================================================
- * Initial state generation
+ * State access methods
  * ========================================================================= */
 
 	public static State getNewState(Domain d) {
@@ -67,7 +81,7 @@ public class POMDPDiaperDomain implements DomainGenerator {
 		ObjectInstance stateE = new ObjectInstance(mentalStateClass, P.OBJ_STATE_E);
 		ObjectInstance stateY = new ObjectInstance(mentalStateClass, P.OBJ_STATE_Y);
 
-		holder.addRelationalTarget(P.ATTR_MENTAL_STATE, stateX.getName());	
+		holder.addRelationalTarget(P.ATTR_MENTAL_STATE, P.OBJ_STATE_X);	
 
 		addObjects(s, holder, stateX, stateA, stateB, stateC, stateD, stateE, stateY);	
 
@@ -78,6 +92,72 @@ public class POMDPDiaperDomain implements DomainGenerator {
 		for(ObjectInstance object : objs) {
 			s.addObject(object);
 		}
+	}
+
+	public static List<State> getAllStates(Domain d) {
+		List<State> ret_val = new ArrayList<State>();
+
+		State s1 = new State();
+		State s2 = new State();
+		State s3 = new State();
+		State s4 = new State();
+		State s5 = new State();
+		State s6 = new State();
+		State s7 = new State();
+
+		ret_val.add(s1);
+		ret_val.add(s2);
+		ret_val.add(s3);
+		ret_val.add(s4);
+		ret_val.add(s5);
+		ret_val.add(s6);
+		ret_val.add(s7);
+
+		ObjectClass stateHolderClass = d.getObjectClass(P.CLASS_STATE_HOLDER);
+		ObjectClass mentalStateClass = d.getObjectClass(P.CLASS_MENTAL_STATE);
+
+		ObjectInstance stateX = new ObjectInstance(mentalStateClass, P.OBJ_STATE_X);
+		ObjectInstance stateA = new ObjectInstance(mentalStateClass, P.OBJ_STATE_A);
+		ObjectInstance stateB = new ObjectInstance(mentalStateClass, P.OBJ_STATE_B);
+		ObjectInstance stateC = new ObjectInstance(mentalStateClass, P.OBJ_STATE_C);
+		ObjectInstance stateD = new ObjectInstance(mentalStateClass, P.OBJ_STATE_D);
+		ObjectInstance stateE = new ObjectInstance(mentalStateClass, P.OBJ_STATE_E);
+		ObjectInstance stateY = new ObjectInstance(mentalStateClass, P.OBJ_STATE_Y);
+
+		for(int i = 0; i < ret_val.size(); ++i) {
+			State temp = ret_val.get(i);
+			addObjects(temp, stateX, stateA, stateB, stateC, stateD, stateE, stateY);
+		}
+
+		ObjectInstance sh1 = new ObjectInstance(stateHolderClass, P.OBJ_HOLDER);
+		sh1.addRelationalTarget(P.ATTR_MENTAL_STATE, P.OBJ_STATE_X);
+		s1.addObject(sh1);
+
+		ObjectInstance sh2 = new ObjectInstance(stateHolderClass, P.OBJ_HOLDER);
+		sh2.addRelationalTarget(P.ATTR_MENTAL_STATE, P.OBJ_STATE_A);
+		s2.addObject(sh2);
+
+		ObjectInstance sh3 = new ObjectInstance(stateHolderClass, P.OBJ_HOLDER);
+		sh3.addRelationalTarget(P.ATTR_MENTAL_STATE, P.OBJ_STATE_B);
+		s3.addObject(sh3);
+
+		ObjectInstance sh4 = new ObjectInstance(stateHolderClass, P.OBJ_HOLDER);
+		sh4.addRelationalTarget(P.ATTR_MENTAL_STATE, P.OBJ_STATE_C);
+		s4.addObject(sh4);
+
+		ObjectInstance sh5 = new ObjectInstance(stateHolderClass, P.OBJ_HOLDER);
+		sh5.addRelationalTarget(P.ATTR_MENTAL_STATE, P.OBJ_STATE_D);
+		s5.addObject(sh5);
+
+		ObjectInstance sh6 = new ObjectInstance(stateHolderClass, P.OBJ_HOLDER);
+		sh6.addRelationalTarget(P.ATTR_MENTAL_STATE, P.OBJ_STATE_E);
+		s6.addObject(sh6);
+
+		ObjectInstance sh7 = new ObjectInstance(stateHolderClass, P.OBJ_HOLDER);
+		sh7.addRelationalTarget(P.ATTR_MENTAL_STATE, P.OBJ_STATE_Y);
+		s7.addObject(sh7);
+
+		return ret_val;
 	}
 
 /* ============================================================================
@@ -106,7 +186,7 @@ public class POMDPDiaperDomain implements DomainGenerator {
 	}
 
 /* ============================================================================
- * Action definition
+ * Speak action definition
  * ========================================================================= */
 
 	public class SpeakAction extends Action {
@@ -116,9 +196,67 @@ public class POMDPDiaperDomain implements DomainGenerator {
 
 		@Override
 		public State performActionHelper(State st, String[] params) {
-			return st;
+			POMDPState ps = new POMDPState(st);
+			POMDPDomain dom = (POMDPDomain) domain;
+
+			ObjectInstance holder = s.getObject(P.OBJ_HOLDER);
+			String state = (String) holder.getAllRelationalTargets(P.ATTR_MENTAL_STATE).toArray()[0];
+
+			switch(state) {
+				case P.OBJ_STATE_X:
+					ps.setObservation(dom.getObservation(P.OBS_STATE_X));
+					break;
+				case P.OBJ_STATE_A:
+					ps.setObservation(dom.getObservation(P.OBS_STATE_A));
+					break;
+				case P.OBJ_STATE_B:
+					ps.setObservation(dom.getObservation(P.OBS_STATE_B));
+					break;
+				case P.OBJ_STATE_C:
+					ps.setObservation(dom.getObservation(P.OBS_STATE_C));
+					break;
+				case P.OBJ_STATE_D:
+					ps.setObservation(dom.getObservation(P.OBS_STATE_D));
+					break;
+				case P.OBJ_STATE_E:
+					ps.setObservation(dom.getObservation(P.OBS_STATE_E));
+					break;
+				case P.OBJ_STATE_Y:
+					ps.setObservation(dom.getObservation(P.OBS_STATE_Y));
+					break;
+			}
+
+			return ps;
+		}
+
+		@Override
+		public List<TransitionProbability> getTransitions(State s, String[] params) {
+			List<TransitionProbability> trans = new ArrayList<TransitionProbability>();
+
+			List<State> states = POMDPDiaperDomain.getAllStates(domain);
+
+			ObjectInstance holder = s.getObject(P.OBJ_HOLDER);
+			String holder_state = (String) holder.getAllRelationalTargets(P.ATTR_MENTAL_STATE).toArray()[0];
+
+			for(State test_state : states) {
+				
+				ObjectInstance test_holder = current_state.getObject(P.OBJ_HOLDER);
+				String test_mental_state = (String) holder.getAllRelationalTargets(P.ATTR_MENTAL_STATE).toArray()[0];
+
+				if(holder_state.equals(test_mental_state)) {
+					trans.add(new TransitionProbability(test_state, 1));
+				} else {
+					trans.add(new TransitionProbability(test_state, 0));
+				}
+			}
+
+			return trans;
 		}
 	}
+
+/* ============================================================================
+ * Advance action definition
+ * ========================================================================= */
 
 	public class AdvanceAction extends Action {
 		public AdvanceAction(Domain domain, String name) {
@@ -164,6 +302,28 @@ public class POMDPDiaperDomain implements DomainGenerator {
 			}
 
 			return st;
+		}
+
+		@Override
+		public List<TransitionProbability> getTransitions(State s, String[] params) {
+			List<TransitionProbability> trans = new ArrayList<TransitionProbability>();
+			List<State> states = POMDPDiaperDomain.getAllStates(domain);
+
+			ObjectInstance mental_state = st.getObject(params[0]);
+
+			for(State test_state : states) {
+
+				ObjectInstance test_holder = current_state.getObject(P.OBJ_HOLDER);
+				String test_mental_state = (String) holder.getAllRelationalTargets(P.ATTR_MENTAL_STATE).toArray()[0];
+
+				if(test_mental_state.equals(mental_state)) {
+					trans.put(new TransitionProbability(test_state, 1));
+				} else {
+					trans.put(new TransitionProbability(test_state, 0));
+				}
+			}
+
+			return trans;
 		}
 	}
 }
